f3d2c91729f5fcf6afc897c228a688938e166e70
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2023-07-28 11:22:52 -0300

    Site psidevs
    Site para fins educacionais

---
+++ js/formulario.js
@@ -0,0 +1,30 @@
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Sua Menssagem foi Enviada!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Algo Deu Errado Tente Novamente!"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Algo Deu Errado Tente Novamente!"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
